1bad9eec57e04ee79ad1004334d2d0240c0862f2
MR.INFLATED <ralfjiang2014@gmail.com>
2017-06-08 04:29:34 +0800

    v0.1

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SrtTranslate
-Translate srt subtitles through Google Translation API
+## Introduction
+Well, it's just a python script to translate your srt subtitle to another language.
+
+## Requirements
+`requests,srt,google.cloud`
+And your Google API key!
+
+## Usage
+
+`python3 process.py PATH SOURCE_LAN TARGET_LAN`
---
+++ process.py
@@ -0,0 +1,45 @@
+import srt,requests,json,sys
+from google.cloud import translate
+
+
+APIKey = "" #Here is your API key
+Url = "https://translation.googleapis.com/language/translate/v2?key="
+
+def translate(queryString,source,target):
+    requestURL = Url  + APIKey + "&source=" + source + "&target=" + target + "&q=" + queryString  
+    response = requests.get(requestURL)
+    translation = json.loads(response.text)["data"]["translations"][0]['translatedText']
+    return translation
+
+
+
+
+
+def process(path,source,target):
+    with open(path,'rt',encoding='gbk',errors='ignore') as f:
+        data = f.read()
+        f.close()
+    
+    subs = list(srt.parse(data))
+
+    for k in subs:
+        text = k.content
+        translation = translate(text,'en','zh-CN')
+
+        k.content = translation
+        sys.stdout.write("Line [%d/%d] completed!\r"%(k.index,len(subs)))
+        sys.stdout.flush()
+        #print("Line[%d/%d]:",k.index,"completed!")
+    srtTranslated = srt.compose(subs)
+
+    # write the srt file translated...
+    with open("Translated.srt",'xt') as f:
+        f.write(srtTranslated)
+        f.close()
+
+if __name__ == "__main__":
+    process(sys.argv[1],sys.argv[2],sys.argv[3])
+
+
+
+
